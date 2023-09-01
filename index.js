3f19beb0c97f11264a53d4ff6661691598626128
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-09-01 09:02:59 +0100

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    const configuration = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: `${name}`,
+        email: `${email}`,
+      }),
+    };
+  
+    return fetch('http://localhost:3000/users', configuration)
+      .then(response => response.json())
+      .then(data => {
+        const emailElement = document.createElement('p')
+        const nameElement = document.createElement('p');
+        emailElement.innerText = data.email
+        nameElement.innerText = data.name
+        document.body.innerHTML = data.id 
+    })
+      .catch(error => {
+        document.body.innerHTML = error.message
+      });
+  }
+
+submitData()
+  
+
+  
